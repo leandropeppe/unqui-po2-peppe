48a297cac4372d869beca656c90580abe3700535
LeandroPeppeTasa <117114218+LeandroPeppeTasa@users.noreply.github.com>
2023-03-27 19:32:34 -0300

    Base tp2

---
+++ src/ar/edu/unq/po2/tp2/Empresa.java
@@ -0,0 +1,21 @@
+package ar.edu.unq.po2.tp2;
+
+public class Empresa {
+	private String nombre;
+
+	public Empresa(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+}
