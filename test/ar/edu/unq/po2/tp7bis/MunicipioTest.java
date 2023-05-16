973d62bf469dfc4334e645cbda743aba06637c0a
leandropeppe <leandropeppe@gmail.com>
2023-05-15 21:57:18 -0300

    Todos los test andando en municipio. Falta solamente un filtro.

--- test/ar/edu/unq/po2/tp7bis/MunicipioTest.java
+++ test/ar/edu/unq/po2/tp7bis/MunicipioTest.java
@@ -1,6 +1,7 @@
 package ar.edu.unq.po2.tp7bis;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -14,6 +15,7 @@ class MunicipioTest {
 	ActividadSemanal futbol11;
 	ActividadSemanal papiFutbol;
 	ActividadSemanal jabalina;
+	ActividadSemanal tenis;
 	
 	
 	
@@ -24,34 +26,47 @@ class MunicipioTest {
 		futbol11 = new ActividadSemanal(DiaDeLaSemana.Jueves,15,1,Deporte.Futbol);
 		papiFutbol = new ActividadSemanal(DiaDeLaSemana.Lunes,12,1,Deporte.Futbol);
 		jabalina = new ActividadSemanal(DiaDeLaSemana.Lunes,20,1,Deporte.Jabalina);
+		tenis = new ActividadSemanal(DiaDeLaSemana.Lunes,20,2,Deporte.Tenis);
 		
 		quilmes.agregarActividadSemanal(futbol5);
 		quilmes.agregarActividadSemanal(futbol11);
 		quilmes.agregarActividadSemanal(papiFutbol);
 		quilmes.agregarActividadSemanal(jabalina);
+		quilmes.agregarActividadSemanal(tenis);
 	}
 
 	@Test
 	void soloFutbolTest() {
-		assertTrue(futbol5.toString()==quilmes.soloFutbol().get(0).toString());
+		equals(futbol5.equals(quilmes.soloFutbol().get(0)));
+		equals(futbol11.equals(quilmes.soloFutbol().get(1)));
+		equals(papiFutbol.equals(quilmes.soloFutbol().get(2)));
+		assertTrue(quilmes.soloFutbol().size()==3);
 	}
 	
 	
 	@Test
 	void cantidadHorasTotalesTest() {
-		assertEquals(4,quilmes.cantidadHorasTotales());
+		assertEquals(6,quilmes.cantidadHorasTotales());
 	}
 	
 	
 	@Test
 	void actividadesDeXComplejidadTest() {
-		//assertEquals(/* lista con solo jabalina */,quilmes.actividadesDeXComplejidad(4));
+		equals(jabalina.equals(quilmes.actividadesDeXComplejidad(4).get(0)));
+		
+		equals(tenis.equals(quilmes.actividadesDeXComplejidad(3).get(0)));
+		
+		assertTrue(quilmes.actividadesDeXComplejidad(1).isEmpty());
 	}
 	
 	
 	@Test
 	void actividadDeMenorCostoTest() {
-		//assertTrue(quilmes.actividadDeMenorCosto(Deporte.Futbol));
+		equals(futbol5.equals(quilmes.actividadDeMenorCosto(Deporte.Futbol)));
+		equals(tenis.equals(quilmes.actividadDeMenorCosto(Deporte.Tenis)));
+		equals(jabalina.equals(quilmes.actividadDeMenorCosto(Deporte.Jabalina)));
+		
+		assertTrue(quilmes.actividadDeMenorCosto(Deporte.Running).isEmpty());
 	}
 	
 	
